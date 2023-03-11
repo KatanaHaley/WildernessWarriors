c9a61c7804479a45f46885ccb1886c75766c2db5
KatanaHaley <haleykatana@gmail.com>
2023-03-10 20:02:31 -0600

    edit and style footer

--- components/FooterBanner.js
+++ components/FooterBanner.js
@@ -3,28 +3,35 @@ import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
 
-const FooterBanner = ({ footerBanner :{discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image}  }) => {
+const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
         <div className="left">
-          <p>{discount}</p>
+          <h2>{discount} until {saleTime}</h2>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
-          <p>{saleTime}</p>
-        </div>
-        <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
+          <div className="footer-product-desc">
           <p>{desc}</p>
+          </div>
+
+          
+
+        </div>
+        <img 
+          src={urlFor(image)} className="footer-banner-img"
+          // width="672"
+          // height="378"   
+        />
+        <div className="right">
+          <h3>{midText}</h3>
+          <p>{smallText}</p>
+
           <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
           </Link>
         </div>
 
-        <img 
-          src={urlFor(image)} className="footer-banner-image"
-        />
       </div>
     </div>
   )
