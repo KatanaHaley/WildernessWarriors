28c64191dcc8b92ccc1b9c83a87c2375645a5ea6
KatanaHaley <haleykatana@gmail.com>
2023-03-10 19:06:53 -0600

    edit hero component

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -8,25 +8,22 @@ const HeroBanner = ({ heroBanner }) => {
     <div className="hero-banner-container">
       
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h1>{heroBanner.largeText2}</h1>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        {/* <h1>{heroBanner.largeText2}</h1> */}
 
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
-
-        <div>
-          <Link href={`/product/${heroBanner.product}`}>
+        <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
-          <div className="desc">
-            <h5>Immersive VR Headset with 3D Audio and Controller</h5>
-            <p>{heroBanner.desc}</p>
-            {/* <p>Experience virtual reality with a comfortable headset</p> */}
-          </div>
-        
-        </div>
 
+        <div>
+          <div className="desc">
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+
+            <h5>{heroBanner.desc}</h5>
+          </div>        
+        </div>
       </div>
     </div>
   )
