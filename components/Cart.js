149b7bfaa22f6492069f2fc28505f492c858d3d9
KatanaHaley <haleykatana@gmail.com>
2023-03-13 15:09:30 -0500

    cleaned up product image styling and cart quantity styling

--- components/Cart.js
+++ components/Cart.js
@@ -82,7 +82,7 @@ const Cart = () => {
                    className="remove-item"
                    onClick={() => onRemove(item)}
                    >
-                    <TiDeleteOutline />
+                    <TiDeleteOutline size="28"/>
                    </button>
                  </div> 
               </div>  
